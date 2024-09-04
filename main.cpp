1e68a3f885514ed37c34bd25440167f77a8d47bf
markova-anna <markova.anna@phystech.edu>
2024-09-05 00:47:42 +0300

    strcpy

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <assert.h>
 
-int mstrcmp(const char* str1, const char* str2);
+void strcpy(char* str1, const char* str2);
 
 int main ()
 {
-    const char* str1 = "cool";
-    const char* str2 = "coold"; 
-    printf("%d", mstrcmp(str1, str2));
+    char str1[] = "";
+    const char* str2 = "cool"; 
+    //printf("coolguy\n");
+    strcpy(str1, str2);
+    printf("%s", str1);
 
     return 0;
 }
 
-int mstrcmp(const char* str1, const char* str2)
+void strcpy(char* str1, const char* str2)
 {
     assert(str1 != nullptr);
     assert(str2 != nullptr);
     int i = 0;
 
-    do
+    while(str2[i] != '\0')
     {
-        if (str1[i] != str2[i])
-            //printf("the o address %p\n", str[i]);
-            return 1;
-        //printf("check address %d - %p\n", i+1, str1[i]);
+        str1[i] = str2[i];
+        //printf("check symbol %d - %c\n", i+1, str1[i]);
         i++;
-    }while(str1[i-1] != '\0');
-    return 0;
+    }
+    //printf("check symbol %d - %c\n", i+1, str1[i]);
+    str1[i] = str2[i];
 }
